2cf8a1776e9f0f9a2b5ce991297cf19732023f25
ochoyomoses-rgb <ochoyomoses@gmail.com>
2026-05-26 22:56:57 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,25 @@
 <?php
 
-if (!is_dir('data')) {
-    mkdir('data', 0777, true);
-}
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
-$db = new PDO('sqlite:data/tracker.sqlite');
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+$db = new PDO($dsn, $user, $password);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $db->exec("
     CREATE TABLE IF NOT EXISTS visits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         website TEXT,
         ip TEXT,
         page TEXT,
         user_agent TEXT,
         referrer TEXT,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
 ");
